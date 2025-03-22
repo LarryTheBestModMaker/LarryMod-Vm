46ffe3c2c75cc035234c73210fc660b24eb7e769
jwklong <72522395+jwklong@users.noreply.github.com>
2025-03-22 21:21:24 +0000

    fix scripts

--- src/extensions/jg_scripts/index.js
+++ src/extensions/jg_scripts/index.js
@@ -94,7 +94,7 @@ class JgScriptsBlocks {
           opcode: "runBlocks",
           text: "run script [NAME] in [SPRITE]",
           blockType: BlockType.LOOP,
-          branchCount: -1,
+          branches: {},
           branchIconURI: "",
           arguments: {
             NAME: { type: ArgumentType.STRING, defaultValue: "Script1" },
@@ -105,7 +105,7 @@ class JgScriptsBlocks {
           opcode: "runBlocksData",
           text: "run script [NAME] in [SPRITE] with data [DATA]",
           blockType: BlockType.LOOP,
-          branchCount: -1,
+          branches: {},
           branchIconURI: "",
           arguments: {
             NAME: { type: ArgumentType.STRING, defaultValue: "Script1" },
