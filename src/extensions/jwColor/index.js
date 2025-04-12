0198c56337f99c7788fbd6e395f8d672a25d0ecb
jwklong <72522395+jwklong@users.noreply.github.com>
2025-04-12 10:44:39 +0100

    fix mix haha that rhymes

--- src/extensions/jwColor/index.js
+++ src/extensions/jwColor/index.js
@@ -358,7 +358,7 @@ class Extension {
         A = Color.Type.toColor(A).toRGB()
         B = Color.Type.toColor(B).toRGB()
 
-        return Color.Type.fromRGB((A[0] + B[0]) / 2, (A[1] + B[1]) / 2, (A[1] + B[1]) / 2)
+        return Color.Type.fromRGB((A[0] + B[0]) / 2, (A[1] + B[1]) / 2, (A[2] + B[2]) / 2)
     }
 
     getR({COLOR}) {
