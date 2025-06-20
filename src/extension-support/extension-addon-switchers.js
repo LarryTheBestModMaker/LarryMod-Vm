a53aa56e022661e6103659657cef692815a0fb00
Steve0Greatness <steve0greatnessiscool@gmail.com>
2025-06-19 20:05:34 -0700

    Add tests for switches to jg_dev
    Also, slight bugfix probably

--- src/extension-support/extension-addon-switchers.js
+++ src/extension-support/extension-addon-switchers.js
@@ -1,114 +1,136 @@
+const log = require('../util/log');
 const switches = {};
+const parser = new DOMParser();
 
-const noopSwitch = {
-    isNoop: true
+const define_error_noop = (msg) => {
+    log.error(msg);
+    return {
+        msg,
+        isNoop: true,
+        isError: true,
+    };
+};
+
+function get_extension_switches(id, blocks) {
+    let _switches = {};
+    for (let block of blocks) {
+        var blockswitches = block.info.switches;
+        if (!blockswitches) continue;
+        let opcode = block.info.opcode;
+        _switches[opcode] = blockswitches.map(current => {
+            switch (typeof current) {
+                case "object":
+                    break;
+                case "string":
+                    current = { opcode: current };
+                    break;
+                default:
+                    return define_error_noop((typeof current) + " disallowed");
+            }
+
+            if (current.isNoop) {
+                return {
+                    isNoop: true,
+                    msg: current.overwriteText ?? block.info.switchText ?? block.info.text
+                };
+            }
+
+            if (!current.opcode) {
+                return define_error_noop("No defined opcode");
+            }
+
+            let get_block = blocks.find(e => e.info.opcode === current.opcode);
+            if (!get_block) {
+                return define_error_noop(`Block ${current.opcode} doesn't exist`);
+            }
+
+            let createInputs = {};
+            let currargs = current.createArguments ?? {};
+
+            parser.parseFromString(get_block.xml, "text/xml")
+                .querySelectorAll(`[type="${get_block.json.type}"] > value`)
+                .forEach(el => {
+                    let name = el.getAttribute("name");
+                    if (
+                        !!block.info.arguments[name]
+                        && !(current.remapArguments ?? {})[name]
+                    ) return;
+                    if (Object.values(current.remapArguments ?? {}).includes(name)) return;
+
+                    let shadowType = el.getElementsByTagName("shadow")[0].getAttribute("type");
+
+                    let value = (currargs[name] ?? get_block.info.arguments[name].defaultValue ?? "").toString();
+
+                    createInputs[name] = {
+                        shadowType,
+                        value
+                    };
+                });
+
+            const splitInputs = Object.keys(block.info.arguments)
+                .filter(arg =>
+                    !!get_block.info.arguments[arg]
+                    && !!(current.remapArguments ?? {})[arg]
+                );
+
+            const remapShadowType = {};
+
+            parser.parseFromString(block.xml, "text/xml")
+                .querySelectorAll(`[type="${block.json.type}"] > value`)
+                .forEach(el => {
+                    let name = el.getAttribute("name");
+                    if ((current.remapArguments ?? {})[name]) name = current.remapArguments[name];
+                    if (!get_block.info.arguments[name]) return;
+
+                    let shadowType = el.querySelector("shadow")
+                    if (!shadowType) return;
+                    shadowType = shadowType.getAttribute("type");
+
+                    remapShadowType[name] = shadowType;
+                });
+
+            parser.parseFromString(get_block.xml, "text/xml")
+                .querySelectorAll(`[type="${get_block.json.type}"] > value`)
+                .forEach(el => {
+                    let name = el.getAttribute("name");
+                    if (!remapShadowType[name]) return;
+
+                    let shadowType = el.querySelector("shadow");
+                    if (!shadowType) return;
+                    shadowType = shadowType.getAttribute("type");
+
+                    if (remapShadowType[name] == shadowType) {
+                        delete remapShadowType[name];
+                        return;
+                    }
+                    remapShadowType[name] = shadowType;
+                });
+
+            return {
+                opcode: `${id}_${current.opcode}`,
+                msg: current.overwriteText ?? get_block.info.switchText ?? get_block.info.text,
+
+                mapFieldValues: current.remapMenus ?? {},
+                remapInputName: current.remapArguments ?? {},
+
+                createInputs,
+                splitInputs,
+                remapShadowType,
+            };
+        });
+    }
+    return _switches;
 }
 
 function getSwitches({runtime}) {
     var _switches = switches;
     for (let ext of runtime._blockInfo) {
         if (ext.id in _switches) continue;
-        _switches[ext.id] = {};
-        for (let block of ext.blocks) {
-            var blockswitches = block.info.switches;
-            if (!blockswitches) continue;
-            let opcode = block.info.opcode;
-            _switches[ext.id][opcode] = blockswitches.map(current => {
-                if (typeof current === "string") {
-                    current = {opcode: current}
-                } else if (typeof current !== "object") {
-                    return noopSwitch;
-                }
-
-                if ("isNoop" in current && current.isNoop) {
-                    return {
-                        isNoop: true,
-                        msg: current.overwriteText ?? block.info.switchText ?? block.info.text
-                    };
-                }
-
-                if (!("opcode" in current)) {
-                    return noopSwitch;
-                }
-
-                let get_block = ext.blocks.find(e => e.info.opcode === current.opcode);
-                if (!get_block) { // block doesn't exist.
-                    return noopSwitch;
-                }
-
-                let createInputs = {};
-                let currargs = current.createArguments ?? {};
-
-                const parser = new DOMParser();
-
-                parser.parseFromString(get_block.xml, "text/xml")
-                    .querySelectorAll(`[type="${get_block.json.type}"] > value`)
-                    .forEach(el => {
-                        let name = el.getAttribute("name");
-                        if (
-                            !!block.info.arguments[name]
-                            && !(current.remapArguments ?? {})[name]
-                        ) return;
-                        if (Object.values(current.remapArguments ?? {}).includes(name)) return;
-
-                        let shadowType = el.getElementsByTagName("shadow")[0].getAttribute("type");
-
-                        let value = (currargs[name] ?? get_block.info.arguments[name].defaultValue ?? "").toString();
-
-                        createInputs[name] = {
-                            shadowType,
-                            value
-                        };
-                    });
-
-                const splitInputs = Object.keys(block.info.arguments)
-                    .filter(arg => !Object.keys(get_block.info.arguments).includes(arg) && !Object.keys(current.remapArguments ?? {}).includes(arg));
-
-                const remapShadowType = {};
-
-                parser.parseFromString(block.xml, "text/xml")
-                    .querySelectorAll(`[type="${block.json.type}"] > value`)
-                    .forEach(el => {
-                        let name = el.getAttribute("name");
-                        if (!(name in get_block.info.arguments)) return;
-                        let shadowType = el.querySelector("shadow")
-                        if (!shadowType) return;
-                        shadowType = shadowType.getAttribute("type");
-                        remapShadowType[name] = shadowType;
-                    });
-
-                parser.parseFromString(get_block.xml, "text/xml")
-                    .querySelectorAll(`[type="${get_block.json.type}"] > value`)
-                    .forEach(el => {
-                        let name = el.getAttribute("name");
-                        if (!(name in remapShadowType)) return;
-
-                        let shadowType = el.querySelector("shadow");
-                        if (!shadowType) {
-                            return;
-                        }
-                        shadowType = shadowType.getAttribute("type");
-
-                        if (remapShadowType[name] == shadowType) {
-                            delete remapShadowType[name];
-                            return;
-                        }
-                        remapShadowType[name] = shadowType;
-                    });
-
-                return {
-                    opcode: `${ext.id}_${current.opcode}`,
-                    remapInputName: current.remapArguments ?? {},
-                    createInputs,
-                    splitInputs,
-                    remapShadowType,
-                    mapFieldValues: current.remapMenus ?? {},
-                    msg: current.overwriteText ?? get_block.info.switchText ?? get_block.info.text,
-                };
-            });
-        }
+        _switches[ext.id] = get_extension_switches(ext.id, ext.blocks);
     }
     return _switches;
 }
 
 module.exports = getSwitches;
+module.exports.get_extension_switches = get_extension_switches;
+module.exports.noopSwitch = { isNoop: true };
