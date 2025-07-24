5c956e93e6883c83766be6107c03920261aff9cf
jwklong <72522395+jwklong@users.noreply.github.com>
2025-07-24 08:38:34 +0100

    date extension test

---
+++ src/extensions/jwDate/index.js
@@ -0,0 +1,115 @@
+const BlockType = require('../../extension-support/block-type')
+const BlockShape = require('../../extension-support/block-shape')
+const ArgumentType = require('../../extension-support/argument-type')
+const Cast = require('../../util/cast')
+
+function span(text) {
+    let el = document.createElement('span')
+    el.innerHTML = text
+    el.style.display = 'hidden'
+    el.style.whiteSpace = 'nowrap'
+    el.style.width = '100%'
+    el.style.textAlign = 'center'
+    return el
+}
+
+class DateType {
+    customId = "jwDate"
+
+    date = new Date(0)
+
+    constructor(date = new Date(0)) {
+        this.date = date
+    }
+
+    static from(x) {
+        if (x instanceof DateType) return new DateType(x.date)
+        if (x instanceof Date) return new DateType(x)
+        if (typeof x == 'string' || typeof x == 'number') return new DateType(new Date(x))
+        return new DateType()
+    }
+
+    jwArrayHandler() {
+        return this.date.toLocaleDateString()
+    }
+
+    toString() {
+        return this.date.toLocaleString()
+    }
+    toMonitorContent = () => span(this.toString())
+
+    toReporterContent() {
+        let root = document.createElement('div')
+        root.style.display = 'flex'
+        root.style.flexDirection = 'column'
+        root.style.justifyContent = 'center'
+
+        root.appendChild(span(this.date.toLocaleDateString()))
+        root.appendChild(span(this.date.toLocaleTimeString()))
+
+        return root
+    }
+}
+
+const jwDate = {
+    Type: DateType,
+    Block: {
+        blockType: BlockType.REPORTER,
+        blockShape: BlockShape.TICKET,
+        forceOutputType: "jwDate",
+        disableMonitor: true
+    },
+    Argument: {
+        shape: BlockShape.TICKET,
+        check: ["jwDate"]
+    },
+    ExpantaNum
+}
+
+class Extension {
+    constructor() {
+        vm.jwDate = jwDate
+        vm.runtime.registerSerializer(
+            "jwDate",
+            v => v.number.toJSON(),
+            v => {
+                let x = new ExpantaNum(0)
+                try {
+                    x = ExpantaNum.fromJSON(v)
+                } catch {}
+                return new jwDate.Type(x)
+            }
+        )
+    }
+
+    getInfo() {
+        return {
+            id: "jwDate",
+            name: "Dates",
+            color1: "#ff788a",
+            blocks: [
+                {
+                    opcode: 'now',
+                    text: 'now',
+                    ...jwDate.Block
+                },
+                {
+                    opcode: 'epoch',
+                    text: 'unix epoch',
+                    ...jwDate.Block
+                }
+            ],
+            menus: {}
+        }
+    }
+
+    now() {
+        return jwDate.Type.from(Date.now())
+    }
+
+    epoch() {
+        return jwDate.Type.from(0)
+    }
+}
+
+module.exports = Extension
