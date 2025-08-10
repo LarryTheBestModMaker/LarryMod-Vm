4acc7e30dcbe4a18e64b97f0250d7c606eeba017
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-08-10 11:46:34 -0700

    vm.blocks.js -- auto stop threads of deleted blocks

--- src/engine/blocks.js
+++ src/engine/blocks.js
@@ -460,9 +460,15 @@ class Blocks {
                 this._blocks[e.blockId].shadow) {
                 return;
             }
-            // Inform any runtime to forget about glows on this script.
+            // If this block is the initial block of a script, inform any runtime to forget about glows
+            // as well as force end the script (if in compiler)
             if (this._blocks[e.blockId].topLevel) {
-                this.runtime.quietGlow(e.blockId);
+                if (this.runtime.compilerOptions.enabled) {
+                    const thread = this.runtime.threads.find(t => t.getId() === `${editingTarget.id}&${e.blockId}`);
+                    if (thread) this.runtime._stopThread(thread);
+                } else {
+                    this.runtime.quietGlow(e.blockId);
+                }
             }
             this.deleteBlock(e.blockId);
             break;
