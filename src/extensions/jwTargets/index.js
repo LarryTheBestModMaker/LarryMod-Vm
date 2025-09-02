b926d8688b515c29cc938ce2f84084ba40fe26df
jwklong <72522395+jwklong@users.noreply.github.com>
2025-09-02 16:23:01 +0100

    fix target array stuff

--- src/extensions/jwTargets/index.js
+++ src/extensions/jwTargets/index.js
@@ -14,6 +14,16 @@ function span(text) {
     return el
 }
 
+const escapeHTML = unsafe => {
+    return unsafe
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("'", "&#039;")
+};
+
+
 class jwTargetType {
     customId = "jwTargets"
 
@@ -31,9 +41,8 @@ class jwTargetType {
 
     jwArrayHandler() {
         try {
-            return `Target<${this.target.sprite.name}>`
-        } catch (e) {
-            console.debug(e, this)
+            return escapeHTML(`Target<${this.target.sprite.name}>`)
+        } catch {
             return `Target`
         }
     }
