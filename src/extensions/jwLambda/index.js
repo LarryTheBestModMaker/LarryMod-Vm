6fb4ef5a68c4515c4224c58b8a35c7e831d34206
jwklong <72522395+jwklong@users.noreply.github.com>
2025-09-05 16:07:16 +0100

    more lambda polish

--- src/extensions/jwLambda/index.js
+++ src/extensions/jwLambda/index.js
@@ -20,8 +20,8 @@ function span(text) {
     let el = document.createElement('span')
     el.innerHTML = text
     el.style.display = 'hidden'
-    el.style.whiteSpace = 'nowrap'
     el.style.width = '100%'
+    el.style.boxSizing = 'border-box'
     el.style.textAlign = 'center'
     return el
 }
@@ -56,14 +56,9 @@ class LambdaType {
     toReporterContent() {
         let root = span(this.toString())
         root.style.display = "block"
-        root.style.maxWidth = "600px"
         root.style.textAlign = "left"
-        root.style.backgroundColor = "#222"
-        root.style.padding = "4px"
-        root.style.borderRadius = "4px"
         root.style.fontFamily = "monospace"
         root.style.fontSize = "14px"
-        root.style.color = "#fff"
         return root
     }
 
@@ -115,6 +110,7 @@ class Extension {
             id: "jwLambda",
             name: "Lambda",
             color1: "#c71a4b",
+            menuIconURI: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMCAyMCI+CiAgPGVsbGlwc2Ugc3R5bGU9ImZpbGw6IHJnYigxOTksIDI2LCA3NSk7IHN0cm9rZTogcmdiKDE1OSwgMjAsIDYwKTsiIGN4PSIxMCIgY3k9IjEwIiByeT0iOS41IiByeD0iOS41Ij48L2VsbGlwc2U+CiAgPHBhdGggZD0iTSA3LjIzNyA1LjI2NCBDIDEwLjM5NSA1LjI2NCAxMC4zOTUgMTQuNzM2IDEzLjU1MSAxNC43MzYgTSAxMC4wNzkgOS4wNTMgTCA2LjQ0OSAxNC43MzYiIHN0eWxlPSJmaWxsOiBub25lOyBzdHJva2U6IHJnYigyNTUsIDI1NSwgMjU1KTsgc3Ryb2tlLWxpbmVjYXA6IHJvdW5kOyBzdHJva2Utd2lkdGg6IDJweDsiPjwvcGF0aD4KPC9zdmc+",
             blocks: [
                 {
                     opcode: 'arg',
