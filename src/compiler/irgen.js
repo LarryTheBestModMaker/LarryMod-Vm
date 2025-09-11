63af08d740d3b6bf073196b7a76083a2e412e41b
jwklong <72522395+jwklong@users.noreply.github.com>
2025-09-11 17:08:07 +0100

    move debug to stg

--- src/compiler/irgen.js
+++ src/compiler/irgen.js
@@ -90,6 +90,8 @@ class ScriptTreeGenerator {
         this.variableCache = {};
 
         this.usesTimer = false;
+
+        this.debug = this.runtime.debug;
     }
 
     setProcedureVariant (procedureVariant) {
@@ -2418,8 +2420,6 @@ class IRGenerator {
         this.procedures = {};
 
         this.analyzedProcedures = [];
-        
-        this.debug = this.blocks.runtime.debug;
     }
 
     static _extensionIRInfo = {};
@@ -2458,9 +2458,6 @@ class IRGenerator {
     generateScriptTree (generator, topBlockId) {
         const result = generator.generate(topBlockId);
         this.addProcedureDependencies(result.dependedProcedures);
-        if (this.debug) {
-            log.info(`IR: script tree compiled`, result);
-        }
         return result;
     }
 
@@ -2527,6 +2524,10 @@ class IRGenerator {
         const ir = new IntermediateRepresentation();
         ir.entry = entry;
         ir.procedures = this.procedures;
+
+        if (this.debug) {
+            log.info(`IR: script tree compiled`, ir);
+        }
         return ir;
     }
     
