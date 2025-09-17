0c59b3d33fd5820212776582eb4625580b3e0654
jwklong <72522395+jwklong@users.noreply.github.com>
2025-09-17 17:44:07 +0100

    fix

--- src/extensions/jwArray/index.js
+++ src/extensions/jwArray/index.js
@@ -221,7 +221,7 @@ class Extension {
                 if (node.compilerInfo && node.compilerInfo.jwArrayUnmodified === true) return oldDescendInput.call(this, node, visualReport)
                 const isCompat = node.kind === "compat"
 
-                const goodThing = x => typeof x == 'object' && x !== null && x.kind
+                const goodThing = x => typeof x == 'object' && x !== null && x.kind && !(x.compilerInfo && x.compilerInfo.jwArrayUnmodified === true)
                 const nodeArg = "{" + Object.entries(isCompat ? node.inputs : node).filter(x => goodThing(x[1])).map(x => `${JSON.stringify(x[0])}: ${descendInput.call(this, x[1]).asUnknown()}`).join(", ") + "}"
                 if (node.kind !== "visualReport") node = Object.fromEntries(Object.entries(node).map(x => [x[0], goodThing(x[1]) ? `node.${x[0]}` : x[1]]))
                 if (isCompat) node.inputs = Object.fromEntries(Object.entries(node.inputs).map(x => [x[0], goodThing(x[1]) ? `node.${x[0]}` : x[1]]))
