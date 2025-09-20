bc0541742ed28a4cbf8d72b79da5cd13e7b06b97
jwklong <72522395+jwklong@users.noreply.github.com>
2025-09-20 16:16:51 +0100

    try this

--- src/compiler/irgen.js
+++ src/compiler/irgen.js
@@ -1661,7 +1661,10 @@ class ScriptTreeGenerator {
             return {
                 kind: 'procedures.return',
                 return: this.descendInputOfBlock(block, 'return'),
-                isDefineClicked: topBlock ? topBlock.opcode === "procedures_return" || topBlock.opcode.startsWith("procedures_definition") : false
+                isDefineClicked: topBlock ? topBlock.opcode === "procedures_return" || topBlock.opcode.startsWith("procedures_definition") : false,
+                compilerInfo: {
+                    jwArrayUnmodified: true
+                }
             };
         }
         case 'procedures_set': 
