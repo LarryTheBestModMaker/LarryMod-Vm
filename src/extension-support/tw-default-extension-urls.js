101d49883494aa8a65951fc3fa88349750090b7f
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2025-10-09 07:08:26 -0500

    let it snow

--- src/extension-support/tw-default-extension-urls.js
+++ src/extension-support/tw-default-extension-urls.js
@@ -1,7 +1,7 @@
 // If a project uses an extension but does not specify a URL, it will default to
 // the URLs given here, if it exists. This is useful for compatibility with other mods.
 
-const defaults = {
+let defaults = {
     // Box2D (`griffpatch`) is not listed here because our extension is not actually
     // compatible with the original version due to fields vs inputs.
 
